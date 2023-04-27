5c92c5d51e38d54fec8f75f401f390eae20bf4d6
hiranmai19 <CS20B014@iittp.ac.in>
2023-04-27 15:14:15 +0530

    small updates

--- frontend/src/components/Route/ProductDetailsCard/ProductDetailsCard.jsx
+++ frontend/src/components/Route/ProductDetailsCard/ProductDetailsCard.jsx
@@ -102,6 +102,7 @@ const ProductDetailsCard = ({ setOpen, data }) => {
                     </div>
                   </Link>
                 </div>
+
                 <h5 className="text-[16px] text-[red] mt-5">(50) Sold out</h5>
               </div>
 
@@ -113,10 +114,10 @@ const ProductDetailsCard = ({ setOpen, data }) => {
 
                 <div className="flex pt-3">
                   <h4 className={`${styles.productDiscountPrice}`}>
-                    {data.discountPrice}$
+                    {data.discountPrice}₹
                   </h4>
                   <h3 className={`${styles.price}`}>
-                    {data.originalPrice ? data.originalPrice + "$" : null}
+                    {data.originalPrice ? data.originalPrice + "₹" : null}
                   </h3>
                 </div>
                 <div className="flex items-center mt-12 justify-between pr-3">
